047cd4ea6c9055bf3551f69523c80d356b5c98b1
Amy Ago <agoa1@hawkmail.newpaltz.edu>
2018-02-11 15:29:59 -0500

    Initial commit

---
+++ prog1.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+
+int main()
+{
+    using namespace std;
+
+    int a=12;
+    int b=10;
+
+    if(a==b)
+    {
+            cout<<"equal"<< endl;
+    }
+    else if(a>b)
+    {
+      cout<<"large"<< endl;
+    }
+    else if(a<b)
+    {
+      cout<<"small"<< endl;
+    }
+
+    return 0;
+}
